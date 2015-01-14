406cad74dffccd2dfa4295778d8fe82f07296569
rzymek <rzymek@gmail.com>
2015-01-14 23:03:15 +0100

    getCap: generate geoportal wmts json description

---
+++ capabilities/orto.js
@@ -0,0 +1,685 @@
+var orto = {
+ "name": "ORTOFOTOMAPA",
+ "format": "image/jpeg",
+ "tiles": {
+  "EPSG_2180": [
+   {
+    "scaleDenominator": 7559523.809523809,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1,
+     "height": 1
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 0
+     },
+     "col": {
+      "min": 0,
+      "max": 0
+     }
+    }
+   },
+   {
+    "scaleDenominator": 3779761.9047619044,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2,
+     "height": 2
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 1
+     },
+     "col": {
+      "min": 0,
+      "max": 1
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889880.9523809522,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 3,
+     "height": 3
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 2
+     },
+     "col": {
+      "min": 0,
+      "max": 2
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944940.4761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 6,
+     "height": 6
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 5
+     },
+     "col": {
+      "min": 0,
+      "max": 5
+     }
+    }
+   },
+   {
+    "scaleDenominator": 472470.23809523805,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 12,
+     "height": 11
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 10
+     },
+     "col": {
+      "min": 0,
+      "max": 11
+     }
+    }
+   },
+   {
+    "scaleDenominator": 236235.11904761902,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 23,
+     "height": 22
+    },
+    "limits": {
+     "row": {
+      "min": 1,
+      "max": 21
+     },
+     "col": {
+      "min": 1,
+      "max": 22
+     }
+    }
+   },
+   {
+    "scaleDenominator": 94494.04761904762,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 57,
+     "height": 54
+    },
+    "limits": {
+     "row": {
+      "min": 4,
+      "max": 53
+     },
+     "col": {
+      "min": 4,
+      "max": 56
+     }
+    }
+   },
+   {
+    "scaleDenominator": 47247.02380952381,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 114,
+     "height": 108
+    },
+    "limits": {
+     "row": {
+      "min": 9,
+      "max": 107
+     },
+     "col": {
+      "min": 9,
+      "max": 113
+     }
+    }
+   },
+   {
+    "scaleDenominator": 23623.511904761905,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 228,
+     "height": 215
+    },
+    "limits": {
+     "row": {
+      "min": 19,
+      "max": 214
+     },
+     "col": {
+      "min": 19,
+      "max": 227
+     }
+    }
+   },
+   {
+    "scaleDenominator": 9449.404761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 569,
+     "height": 536
+    },
+    "limits": {
+     "row": {
+      "min": 47,
+      "max": 535
+     },
+     "col": {
+      "min": 47,
+      "max": 568
+     }
+    }
+   },
+   {
+    "scaleDenominator": 4724.702380952381,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1137,
+     "height": 1071
+    },
+    "limits": {
+     "row": {
+      "min": 95,
+      "max": 1070
+     },
+     "col": {
+      "min": 95,
+      "max": 1136
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889.8809523809523,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2843,
+     "height": 2676
+    },
+    "limits": {
+     "row": {
+      "min": 239,
+      "max": 2675
+     },
+     "col": {
+      "min": 239,
+      "max": 2842
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944.9404761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 5685,
+     "height": 5352
+    },
+    "limits": {
+     "row": {
+      "min": 479,
+      "max": 5351
+     },
+     "col": {
+      "min": 479,
+      "max": 5684
+     }
+    }
+   },
+   {
+    "scaleDenominator": 472.4702380952381,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 11369,
+     "height": 10704
+    },
+    "limits": {
+     "row": {
+      "min": 959,
+      "max": 10703
+     },
+     "col": {
+      "min": 959,
+      "max": 11368
+     }
+    }
+   }
+  ],
+  "EPSG_4326": [
+   {
+    "scaleDenominator": 7559523.809523809,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2,
+     "height": 1
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 0
+     },
+     "col": {
+      "min": 0,
+      "max": 1
+     }
+    }
+   },
+   {
+    "scaleDenominator": 3779761.9047619044,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 3,
+     "height": 2
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 1
+     },
+     "col": {
+      "min": 0,
+      "max": 2
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889880.9523809522,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 6,
+     "height": 3
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 2
+     },
+     "col": {
+      "min": 0,
+      "max": 5
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944940.4761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 11,
+     "height": 6
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 5
+     },
+     "col": {
+      "min": 1,
+      "max": 10
+     }
+    }
+   },
+   {
+    "scaleDenominator": 472470.23809523805,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 21,
+     "height": 12
+    },
+    "limits": {
+     "row": {
+      "min": 1,
+      "max": 11
+     },
+     "col": {
+      "min": 2,
+      "max": 20
+     }
+    }
+   },
+   {
+    "scaleDenominator": 236235.11904761902,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 42,
+     "height": 24
+    },
+    "limits": {
+     "row": {
+      "min": 3,
+      "max": 23
+     },
+     "col": {
+      "min": 5,
+      "max": 41
+     }
+    }
+   },
+   {
+    "scaleDenominator": 94494.04761904762,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 105,
+     "height": 59
+    },
+    "limits": {
+     "row": {
+      "min": 8,
+      "max": 58
+     },
+     "col": {
+      "min": 14,
+      "max": 104
+     }
+    }
+   },
+   {
+    "scaleDenominator": 47247.02380952381,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 210,
+     "height": 117
+    },
+    "limits": {
+     "row": {
+      "min": 17,
+      "max": 116
+     },
+     "col": {
+      "min": 29,
+      "max": 209
+     }
+    }
+   },
+   {
+    "scaleDenominator": 23623.511904761905,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 419,
+     "height": 234
+    },
+    "limits": {
+     "row": {
+      "min": 35,
+      "max": 233
+     },
+     "col": {
+      "min": 58,
+      "max": 418
+     }
+    }
+   },
+   {
+    "scaleDenominator": 9449.404761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1048,
+     "height": 585
+    },
+    "limits": {
+     "row": {
+      "min": 87,
+      "max": 584
+     },
+     "col": {
+      "min": 146,
+      "max": 1047
+     }
+    }
+   },
+   {
+    "scaleDenominator": 4724.702380952381,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2095,
+     "height": 1169
+    },
+    "limits": {
+     "row": {
+      "min": 175,
+      "max": 1168
+     },
+     "col": {
+      "min": 292,
+      "max": 2094
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889.8809523809523,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 5237,
+     "height": 2922
+    },
+    "limits": {
+     "row": {
+      "min": 439,
+      "max": 2921
+     },
+     "col": {
+      "min": 731,
+      "max": 5236
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944.9404761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 10474,
+     "height": 5843
+    },
+    "limits": {
+     "row": {
+      "min": 879,
+      "max": 5842
+     },
+     "col": {
+      "min": 1463,
+      "max": 10473
+     }
+    }
+   }
+  ]
+ }
+}
---
+++ capabilities/topo.js
@@ -0,0 +1,660 @@
+var topo = {
+ "name": "MAPA TOPOGRAFICZNA",
+ "format": "image/jpeg",
+ "tiles": {
+  "EPSG_2180": [
+   {
+    "scaleDenominator": 7559523.809523809,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1,
+     "height": 1
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 0
+     },
+     "col": {
+      "min": 0,
+      "max": 0
+     }
+    }
+   },
+   {
+    "scaleDenominator": 3779761.9047619044,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2,
+     "height": 2
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 1
+     },
+     "col": {
+      "min": 0,
+      "max": 1
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889880.9523809522,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 3,
+     "height": 3
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 2
+     },
+     "col": {
+      "min": 0,
+      "max": 2
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944940.4761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 6,
+     "height": 6
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 5
+     },
+     "col": {
+      "min": 0,
+      "max": 5
+     }
+    }
+   },
+   {
+    "scaleDenominator": 472470.23809523805,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 12,
+     "height": 12
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 11
+     },
+     "col": {
+      "min": 0,
+      "max": 11
+     }
+    }
+   },
+   {
+    "scaleDenominator": 236235.11904761902,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 24,
+     "height": 23
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 22
+     },
+     "col": {
+      "min": 0,
+      "max": 23
+     }
+    }
+   },
+   {
+    "scaleDenominator": 94494.04761904762,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 60,
+     "height": 57
+    },
+    "limits": {
+     "row": {
+      "min": 1,
+      "max": 56
+     },
+     "col": {
+      "min": 1,
+      "max": 59
+     }
+    }
+   },
+   {
+    "scaleDenominator": 47247.02380952381,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 120,
+     "height": 113
+    },
+    "limits": {
+     "row": {
+      "min": 2,
+      "max": 112
+     },
+     "col": {
+      "min": 3,
+      "max": 119
+     }
+    }
+   },
+   {
+    "scaleDenominator": 23623.511904761905,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 240,
+     "height": 225
+    },
+    "limits": {
+     "row": {
+      "min": 5,
+      "max": 224
+     },
+     "col": {
+      "min": 7,
+      "max": 239
+     }
+    }
+   },
+   {
+    "scaleDenominator": 9449.404761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 598,
+     "height": 562
+    },
+    "limits": {
+     "row": {
+      "min": 14,
+      "max": 561
+     },
+     "col": {
+      "min": 18,
+      "max": 597
+     }
+    }
+   },
+   {
+    "scaleDenominator": 4724.702380952381,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1196,
+     "height": 1123
+    },
+    "limits": {
+     "row": {
+      "min": 29,
+      "max": 1122
+     },
+     "col": {
+      "min": 36,
+      "max": 1195
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889.8809523809523,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2990,
+     "height": 2806
+    },
+    "limits": {
+     "row": {
+      "min": 73,
+      "max": 2805
+     },
+     "col": {
+      "min": 92,
+      "max": 2989
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944.9404761904761,
+    "origin": [
+     850000,
+     100000
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 5980,
+     "height": 5611
+    },
+    "limits": {
+     "row": {
+      "min": 147,
+      "max": 5610
+     },
+     "col": {
+      "min": 184,
+      "max": 5979
+     }
+    }
+   }
+  ],
+  "EPSG_4326": [
+   {
+    "scaleDenominator": 7559523.809523809,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2,
+     "height": 1
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 0
+     },
+     "col": {
+      "min": 0,
+      "max": 1
+     }
+    }
+   },
+   {
+    "scaleDenominator": 3779761.9047619044,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 3,
+     "height": 2
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 1
+     },
+     "col": {
+      "min": 0,
+      "max": 2
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889880.9523809522,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 6,
+     "height": 4
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 3
+     },
+     "col": {
+      "min": 0,
+      "max": 5
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944940.4761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 12,
+     "height": 7
+    },
+    "limits": {
+     "row": {
+      "min": 0,
+      "max": 6
+     },
+     "col": {
+      "min": 0,
+      "max": 11
+     }
+    }
+   },
+   {
+    "scaleDenominator": 472470.23809523805,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 23,
+     "height": 13
+    },
+    "limits": {
+     "row": {
+      "min": 1,
+      "max": 12
+     },
+     "col": {
+      "min": 1,
+      "max": 22
+     }
+    }
+   },
+   {
+    "scaleDenominator": 236235.11904761902,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 45,
+     "height": 25
+    },
+    "limits": {
+     "row": {
+      "min": 2,
+      "max": 24
+     },
+     "col": {
+      "min": 3,
+      "max": 44
+     }
+    }
+   },
+   {
+    "scaleDenominator": 94494.04761904762,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 111,
+     "height": 62
+    },
+    "limits": {
+     "row": {
+      "min": 5,
+      "max": 61
+     },
+     "col": {
+      "min": 9,
+      "max": 110
+     }
+    }
+   },
+   {
+    "scaleDenominator": 47247.02380952381,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 221,
+     "height": 123
+    },
+    "limits": {
+     "row": {
+      "min": 10,
+      "max": 122
+     },
+     "col": {
+      "min": 18,
+      "max": 220
+     }
+    }
+   },
+   {
+    "scaleDenominator": 23623.511904761905,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 442,
+     "height": 245
+    },
+    "limits": {
+     "row": {
+      "min": 21,
+      "max": 244
+     },
+     "col": {
+      "min": 36,
+      "max": 441
+     }
+    }
+   },
+   {
+    "scaleDenominator": 9449.404761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 1104,
+     "height": 613
+    },
+    "limits": {
+     "row": {
+      "min": 54,
+      "max": 612
+     },
+     "col": {
+      "min": 90,
+      "max": 1103
+     }
+    }
+   },
+   {
+    "scaleDenominator": 4724.702380952381,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 2207,
+     "height": 1225
+    },
+    "limits": {
+     "row": {
+      "min": 109,
+      "max": 1224
+     },
+     "col": {
+      "min": 181,
+      "max": 2206
+     }
+    }
+   },
+   {
+    "scaleDenominator": 1889.8809523809523,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 5517,
+     "height": 3061
+    },
+    "limits": {
+     "row": {
+      "min": 273,
+      "max": 3060
+     },
+     "col": {
+      "min": 453,
+      "max": 5516
+     }
+    }
+   },
+   {
+    "scaleDenominator": 944.9404761904761,
+    "origin": [
+     56,
+     12
+    ],
+    "tileSize": {
+     "width": 512,
+     "height": 512
+    },
+    "matrixSize": {
+     "width": 11034,
+     "height": 6121
+    },
+    "limits": {
+     "row": {
+      "min": 547,
+      "max": 6120
+     },
+     "col": {
+      "min": 906,
+      "max": 11033
+     }
+    }
+   }
+  ]
+ }
+}
