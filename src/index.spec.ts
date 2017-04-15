aaf5b0af54535a48e1f1131cc2c2eb2813d6849a
rzymek <rzymek@gmail.com>
2017-04-15 17:31:17 +0200

    test switched to jest

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,5 @@
-import {expect} from 'chai';
-import * as mocha from "mocha";
-
 describe("A test suite", function () {
     it('should be ok', function () {
-        expect(true).to.be.true;
+        expect(true).toBe(true);
     });
 });
--- src/logic/proj4defs.spec.ts
+++ src/logic/proj4defs.spec.ts
@@ -1,5 +1,3 @@
-import {expect} from 'chai';
-import * as mocha from "mocha";
 import {setupProjections} from "./proj4defs";
 import * as proj4 from "proj4";
 import * as _ from "lodash";
@@ -13,8 +11,8 @@ describe(__filename, function () {
         const latLon = proj4("PUWG92", "WGS84", _.clone(puwg));
 
         const back = _.mapValues(proj4("WGS84", "PUWG92", _.clone(latLon)), Math.round);
-        expect(puwg).to.deep.equal(back);
-        expect(_.mapValues(latLon, v => _.round(v, 2))).to.deep.equal({
+        expect(puwg).toEqual(back);
+        expect(_.mapValues(latLon, v => _.round(v, 2))).toEqual({
             x: 12.35,
             y: 52.18
         });
